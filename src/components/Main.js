35127dc11a9872ebbdc610daff7e259ffd220f0c
Michael <m.paspaliaris@gmail.com>
2022-10-30 22:36:15 +1100

    adding styling to main page

---
+++ src/components/Main.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+export default function Main() {
+
+    return (
+        <div className="main--container">
+            <div className="main--logo">
+                <img src={require("../resources/logo2.png")}
+                    className="logo"
+                />
+            </div>
+            <div className="main--left">
+                <h1>Front-end <br />  Developer<span>.</span></h1>
+                <h2>A love for creating detailed and complete frontend packages for a great end user experience.</h2>
+            </div>
+            <div className="main--right">
+                <div className="sub--main">
+                    <img src={require("../resources/headshot.png")}
+                        className="headshot"
+                    />
+                    <img src={require("../resources/whiteframe.png")}
+                        className="whiteframe"
+                    />
+                    <img src={require("../resources/jaggedgreen.png")}
+                        className="jagged--green bot"
+                    />
+                    <img src={require("../resources/jaggedgreen.png")}
+                        className="jagged--green mid"
+                    />
+                    <img src={require("../resources/jaggedpurple.png")}
+                        className="jagged--purple"
+                    />
+                    <img src={require("../resources/greendots.png")}
+                        className="dots--green"
+                    />
+                    <img src={require("../resources/purpledots.png")}
+                        className="dots--purple"
+                    />
+                </div>
+            </div>
+        </div>
+    )
+}
