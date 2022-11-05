4dd7887a2acf580fb22d3d74fd820299aff843fc
Michael <m.paspaliaris@gmail.com>
2022-11-05 17:39:50 +1100

    added image and content to aboutme

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -10,7 +10,28 @@ export default function AboutMe() {
             animate={{ opacity: 1, transition: { duration: 0.5 } }}
             exit={{ opacity: 0 }}
         >
-            <div>
+            <div className="aboutme--container">
+                <div className="aboutme--content">
+                    <h1>Over the past year<span>,</span></h1>
+                    <h3>I've built products for companies and businesses around the globe ranging from marketing websites to complex solutions and enterprise apps with focus on fast, elegant and accessible user experiences.
+
+                        Currently, I work at Shopify as a Senior UX Developer and Accessibility advocate crafting thoughtful and inclusive experiences that adhere to web standards for over a million merchants across the world.
+
+                        Before now, I was Principal Frontend Engineer at hellotax, where I worked on a suite of tools and services tailored at providing fast, automated VAT Registration / filings & Returns solutions for multi-channel sellers across Europe.
+
+                        Prior to hellotax, I was Senior frontend engineering contractor with Pixel2HTML, building JavaScript applications and interfaces for orgs and individuals.
+
+                        I once also led the frontend team at a Russian startup, Conectar through building multiple React applications into a single robust learning platform.
+                    </h3>
+
+                </div>
+                <div>
+                    <img
+                        src={require("../resources/transparency8.png")}
+                        className="aboutme--image"
+                    />
+
+                </div>
 
             </div>
         </motion.div>
