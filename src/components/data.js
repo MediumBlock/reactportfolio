4e34d07bbd699185d00ac7f45d1ae6a7852c68eb
Michael <m.paspaliaris@gmail.com>
2022-11-06 17:48:15 +1100

    added work and tile templates

---
+++ src/components/data.js
@@ -0,0 +1,15 @@
+/*
+websites to display:
+picsome
+opentrivia
+minireddit
+reactportfolio
+typespeed
+moviedatabase (maybe)
+memegenerator (maybe)
+jamming (maybe)
+react-router-lesson
+react-challenge (maybe)
+adopt-a-pet
+
+*/
