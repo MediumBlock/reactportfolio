40e5e140b6e3f16db6b4b392d827ffbc8780f8bf
Michael <m.paspaliaris@gmail.com>
2022-11-14 16:43:06 +1100

    added extra functionality to the button send feature as well as various stylings applied

--- src/components/MessageMe.js
+++ src/components/MessageMe.js
@@ -1,19 +1,21 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import emailjs from '@emailjs/browser';
 import { motion } from "framer-motion";
 
 export default function MessageMe() {
     const form = useRef();
+    const [isSubmitted, setIsSubmitted] = useState(false);
 
     const sendEmail = (e) => {
         e.preventDefault();
 
-        emailjs.sendForm('service_l0ydn4l', 'template_pfrc4lb', form.current, 'Gvck6KUwM1AwGXsPy')
-            .then((result) => {
-                console.log(result.text);
-            }, (error) => {
-                console.log(error.text);
-            });
+        // emailjs.sendForm('service_l0ydn4l', 'template_pfrc4lb', form.current, 'Gvck6KUwM1AwGXsPy')
+        //     .then((result) => {
+        //         console.log(result.text);
+        //     }, (error) => {
+        //         console.log(error.text);
+        //     });
+        setIsSubmitted(true);
     };
 
     return (
@@ -49,10 +51,19 @@ export default function MessageMe() {
                         placeholder='Hi, I would love to get in touch and talk about XY.'
                     />
                 </div>
-                <input className="button"
-                    type="submit"
-                    value="Send"
-                />
+                {isSubmitted ?
+                    <input className="button button--sent"
+                        value="Sent!"
+                        disabled
+
+                    />
+                    :
+                    <input className="button"
+                        type="submit"
+                        value="Send"
+
+                    />
+                }
             </form>
 
         </motion.div>
