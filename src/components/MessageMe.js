162ed359317fd334b39e74d1681807b3bc03ae0a
Michael <m.paspaliaris@gmail.com>
2022-11-14 17:50:58 +1100

    finalised styling and cleaned up code in messageme

--- src/components/MessageMe.js
+++ src/components/MessageMe.js
@@ -21,52 +21,51 @@ export default function MessageMe() {
 
     return (
         <motion.div
-            className='messageme--container'
+            className='messageme--main'
             initial={{ opacity: 0 }}
             animate={{ opacity: 1, transition: { duration: 0.5 } }}
             exit={{ opacity: 0 }}
         >
-            <div className='messageme--title'>
-                <h2>Send me a message!</h2>
-                <h4>Got a question or proposal, or just want
-                    to say hello? Go ahead.</h4>
-            </div>
-            <div className='form--container'>
-                <form ref={form} onSubmit={sendEmail}>
-                    <div className='form--name'>
-                        <label>Your Name</label>
-                        <input type="text"
-                            name="user_name"
-                            placeholder='Enter your name'
-                        />
-                    </div>
-                    <div className='form--email'>
-                        <label>Your Email</label>
-                        <input type="email"
-                            name="user_email"
-                            placeholder='Enter your email address'
-                        />
-                    </div>
-                    <div className='form--message'>
-                        <label>Your Message</label>
-                        <textarea name="message"
-                            placeholder='Hi, I would love to get in touch and talk about XY.'
-                        />
-                    </div>
-                    {isSubmitted ?
-                        <input className="button button--sent"
-                            value="Sent!"
-                            disabled
-
-                        />
-                        :
-                        <input className="button"
-                            type="submit"
-                            value="Send"
-
-                        />
-                    }
-                </form>
+            <div className='messageme--container'>
+                <div className='messageme--title'>
+                    <h2>Send me a message!</h2>
+                    <h4>Got a question or proposal, or just want
+                        to say hello? Go ahead.</h4>
+                </div>
+                <div className='form--container'>
+                    <form ref={form} onSubmit={sendEmail}>
+                        <div className='form--row1'>
+                            <div className='form--name'>
+                                <label>Your Name</label>
+                                <input type="text"
+                                    name="user_name"
+                                    placeholder='Enter your name'
+                                    disabled={isSubmitted}
+                                />
+                            </div>
+                            <div className='form--email'>
+                                <label>Your Email</label>
+                                <input type="email"
+                                    name="user_email"
+                                    placeholder='Enter your email address'
+                                    disabled={isSubmitted}
+                                />
+                            </div>
+                        </div>
+                        <div className='form--message'>
+                            <label>Your Message</label>
+                            <textarea name="message"
+                                placeholder='Hi, I would love to get in touch and talk about XY.'
+                                disabled={isSubmitted}
+                            />
+                        </div>
+                            <input className={isSubmitted ? "button button--sent" : "button" }
+                                value={isSubmitted ? "Sent!" : "Send" }
+                                type="submit"
+                                disabled={isSubmitted}
+                            />
+                    </form>
+                </div>
             </div>
 
         </motion.div>
