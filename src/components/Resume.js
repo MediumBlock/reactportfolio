b93e214239dbe30d772a2b16dcd6e20ab2076cd5
Michael <m.paspaliaris@gmail.com>
2022-11-16 16:59:29 +1100

    saving changes before branching new feature

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -9,32 +9,49 @@ export default function Resume() {
             animate={{ opacity: 1, transition: { duration: 0.5 } }}
             exit={{ opacity: 0 }}
             className="resume--container">
-                <div className="resume--section">
-                    <div className="resume--left">
-                        <div className="contact--left">
-
-                        </div>
-                        <div className="tech--left">
-
-                        </div>
-                        <div className="education--left">
-
-                        </div>
+            <div className="resume--section">
+                <div className="resume--left">
+                    <div className="contact--left">
+                        <h4>Details</h4>
+                        <ul>
+                            <li>Melbourne, Australia</li>
+                            <li>+61 420 686 886</li>
+                            <li>m.paspaliaris@gmail.com</li>
+                        </ul>
                     </div>
-                    <div className="resume--right">
-                        <div className="hero--right">
+                    <div className="tech--left">
+                        <h4>Core Technologies</h4>
+                        <ul>
+                            <li>Javascript</li>
+                            <li>React</li>
+                            <li>CSS</li>
+                            <li>React Router</li>
+                            <li>React Redux</li>
+                        </ul>
 
-                        </div>
-                        <div className="education--right">
-
-                        </div>
-                        <div className="experience--right">
-
-                        </div>
+                    </div>
+                    <div className="education--left">
+                        <h4>Formal Education</h4>
+                        <ul>
+                            <li>Bachelor of Accounting<br />University of South Australia</li>
+                            <li>Bachelor of Mechanical & Manufacturing Engineering <br />University of Melbourne(incomplete)</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="resume--right">
+                    <div className="hero--right">
+                        <h1>Michael Paspa</h1>
+                    </div>
+                    <div className="education--right">
+                        <p>codecademy</p>
+                    </div>
+                    <div className="experience--right">
 
                     </div>
 
                 </div>
+
+            </div>
 
         </motion.div>
     )
