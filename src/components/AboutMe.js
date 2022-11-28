cdd2a326ab51306619f7bb60af0b018d79bde7da
Michael <m.paspaliaris@gmail.com>
2022-11-28 21:50:08 +1100

    updated paragraph content in skills

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -4,8 +4,6 @@ import useAppDirection from "../Hooks/useAppDirection";
 
 
 export default function AboutMe() {
-
-
     const { isUp, location } = useAppDirection("/work", "/skills", 1000, -1000);
 
     return (
@@ -18,15 +16,13 @@ export default function AboutMe() {
             <div className="aboutme--container">
                 <div className="aboutme--content">
                     <h1>Over the past year<span>,</span></h1>
-                    <h3>I've built products for companies and businesses around the globe ranging from marketing websites to complex solutions and enterprise apps with focus on fast, elegant and accessible user experiences.
-
-                        Currently, I work at Shopify as a Senior UX Developer and Accessibility advocate crafting thoughtful and inclusive experiences that adhere to web standards for over a million merchants across the world.
-
-                        Before now, I was Principal Frontend Engineer at hellotax, where I worked on a suite of tools and services tailored at providing fast, automated VAT Registration / filings & Returns solutions for multi-channel sellers across Europe.
-
-                        Prior to hellotax, I was Senior frontend engineering contractor with Pixel2HTML, building JavaScript applications and interfaces for orgs and individuals.
-
-                        I once also led the frontend team at a Russian startup, Conectar through building multiple React applications into a single robust learning platform.
+                    <h3>I started my coding journey on April the 1st 2022, after many years working in different industries, disciplines and countries I found an immediate and profound satisfaction with programming, from my first lesson on April fools day, I found out that my love for coding was indeed not a joke.
+                        <br />
+                        From that day forward I have studied almost religiously never missing a day to try and learn more in my newly adapted craft, from starting with html and css and then moving forward to deep dives into JavaScript, React and all the React Libraries that come with it such as Redux. I have found a constant joy and passion with every new skill I've been able to learn and utilise.
+                        <br />
+                        Every journey begins with a first step, and I am very fortunate to have taken that step as I wouldn't have been able to find a passion that allows me to be so creative and understand something so technical at the same time.
+                        <br />
+                        Utilising learning platforms such as Codecademy and Scrimba I am entirely self-taught and self-motivated, nothing gives me more satisfaction than building a new app or idea, or finding the solution to that one bug in the code that was paining me for hours.
                     </h3>
 
                 </div>
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -20,13 +20,16 @@ export default function Skills() {
                     <img src={require("../resources/green-dots.png")}
                         className="green" />
                     <h1>Design</h1>
-                    <h5>I'm probably not the typical designer positioned behind an Illustrator artboard adjusting pixels, but I design. Immersed in stylesheets tweaking font sizes and contemplating layouts is where you'll find me (~_^). I'm committed to creating fluent user experiences while staying fashionable.</h5>
+                    <h5>Experience in all aspects of creating a visually appealing design across multiple disciplines such as colour theory, typography, visual hierachy, design philosophy and more. 
+                        <br /><br/>Also experienced in achieving the best possible end user experience utilising UX topics such as Interaction / Inclusive Design, Design Psychology, as well as encoporating methods to help speed up and streamline the design process such as Wireframing and Prototyping
+                        <br /><br />All the design principles listed above and more have not only been extensively studied but also put into practise across multiple projects showcased later on.</h5>
                     <img src={require("../resources/skills-left.png")} />
                 </div>
                 <div className="skills--right">
                     <img src={require("../resources/skills-right.png")} />
                     <h1>Engineering</h1>
-                    <h5>In building JavaScript applications, I'm equipped with just the right tools, and can absolutely function independently of them to deliver fast, resilient solutions optimized for scale — performance and scalabilty are priorities on my radar.</h5>
+                    <h5>What's good design without the means to implement? It has been crucial in my development to focus on sharpening my skills on all languages learnt throughout my studies with a particular focus on JavaScript and React. 
+                        <br/> I have made countless apps from scratch to solidify my skills and test my knowledge; whether it is a simple task list app that every developer starts off with to more detailed and complex apps such as reddit clones or even this very portfolio you are viewing now, each app was create with a purpose in mind, but none of them were lacking passion. </h5>
                 </div>
             </div>
             <div>
