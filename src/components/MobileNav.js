9a63a7760c9185136d767e1d2cb7073574d4f33f
Michael <m.paspaliaris@gmail.com>
2022-12-18 01:17:32 +1100

    adding logic for navigating pages in MobileNav component

--- src/components/MobileNav.js
+++ src/components/MobileNav.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { Context } from "../Hooks/Context";
 import { Link, useLocation, useNavigate } from "react-router-dom"
 
@@ -7,11 +7,39 @@ import { Link, useLocation, useNavigate } from "react-router-dom"
 export default function MobileNav() {
 
     const { breakpoint, width } = useContext(Context);
+    const navigate = useNavigate();
     const location = useLocation();
+    const { pathname } = location
+
+    function navUp() {
+        switch (pathname) {
+            case "/skills":
+                return "/";
+                break;
+            case "/aboutme":
+                return "/skills";
+                break;
+            case "/work":
+                return "/aboutme";
+                break;
+            case "/messageme":
+                return "/work";
+                break;
+        }
+    }
+
+
+    console.log("hello", navUp());
+
+
 
     return (
         <div className="mobile--container">
-            <div className="mobile--top">
+            <div className="mobile--top"
+                onClick={
+                    () => navigate("/")
+                }
+            >
                 {
                     location.pathname === "/aboutme"
                         ?
