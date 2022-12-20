eba046f3159c910a079c799c731d5b929ed646eb
Michael <m.paspaliaris@gmail.com>
2022-12-20 18:29:48 +1100

    continued to make main component responsive down to smallest screen sizes

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
 import { motion } from "framer-motion";
 import useAppDirection from "../Hooks/useAppDirection";
+import { Context } from "../Hooks/Context";
 
 
 export default function Main() {
 
     const { isUp, location, navArrows } = useAppDirection("/skills", "/", 1000);
+    const { width, breakpoint } = useContext(Context)
 
 
     return (
@@ -23,9 +25,10 @@ export default function Main() {
                     <h4 className="subheading">Self-motivated and experienced in several disciplines from UI to UX and all the programming needed in between</h4>
                     <h4>Eager to tackle any challenges and skill up front end or back</h4>
                 </div>
-                <img src={require("../resources/line.png")}
-                    className="squiggle"
-                />
+                {breakpoint < width &&
+                    <img src={require("../resources/line.png")}
+                        className="squiggle"
+                    />}
             </div>
             <div className="main--right">
                 <div className="sub--main">
@@ -35,9 +38,13 @@ export default function Main() {
                     <img src={require("../resources/whiteframe.png")}
                         className="whiteframe"
                     />
-                    <img src={require("../resources/mainimg.png")}
+                    {breakpoint < width ? <img src={require("../resources/mainimg.png")}
                         className="main--img"
                     />
+                        :
+                        <img src={require("../resources/mainimg-mobile.png")}
+                            className="main--img"
+                        />}
 
                 </div>
             </div>
