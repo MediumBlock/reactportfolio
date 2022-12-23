e1521aabfd848c068c20d9a2b1384db6545c524e
Michael <m.paspaliaris@gmail.com>
2022-12-23 16:53:43 +1100

    made adjustments to responsive design of main component

--- src/components/Main.js
+++ src/components/Main.js
@@ -18,34 +18,36 @@ export default function Main() {
             exit={{ opacity: 0, y: -1000, transition: { duration: 0.8 } }}
         >
             {navArrows}
-            <div className="main--left">
-                <h1>Front-end <br />  Developer<span>.</span></h1>
-                <h2>A love for creating detailed and complete frontend packages for a great end user experience.</h2>
-                <div className="subtext">
-                    <h4 className="subheading">Self-motivated and experienced in several disciplines from UI to UX and all the programming needed in between</h4>
-                    <h4>Eager to tackle any challenges and skill up front end or back</h4>
-                </div>
-                {breakpoint < width &&
-                    <img src={require("../resources/line.png")}
-                        className="squiggle"
-                    />}
-            </div>
-            <div className="main--right">
-                <div className="sub--main">
-                    <img src={require("../resources/headshot.png")}
-                        className="headshot"
-                    />
-                    <img src={require("../resources/whiteframe.png")}
-                        className="whiteframe"
-                    />
-                    {breakpoint < width ? <img src={require("../resources/mainimg.png")}
-                        className="main--img"
-                    />
-                        :
-                        <img src={require("../resources/mainimg-mobile.png")}
-                            className="main--img"
+            <div className="main--content">
+                <div className="main--left">
+                    <h1>Front-end <br />  Developer<span>.</span></h1>
+                    <h2>A love for creating detailed and complete frontend packages for a great end user experience.</h2>
+                    <div className="subtext">
+                        <h4 className="subheading">Self-motivated and experienced in several disciplines from UI to UX and all the programming needed in between</h4>
+                        <h4>Eager to tackle any challenges and skill up front end or back</h4>
+                    </div>
+                    {breakpoint < width &&
+                        <img src={require("../resources/line.png")}
+                            className="squiggle"
                         />}
+                </div>
+                <div className="main--right">
+                    <div className="sub--main">
+                        <img src={require("../resources/headshot.png")}
+                            className="headshot"
+                        />
+                        <img src={require("../resources/whiteframe.png")}
+                            className="whiteframe"
+                        />
+                        {breakpoint < width ? <img src={require("../resources/mainimg.png")}
+                            className="main--img"
+                        />
+                            :
+                            <img src={require("../resources/mainimg-mobile.png")}
+                                className="main--img"
+                            />}
 
+                    </div>
                 </div>
             </div>
         </motion.div >
