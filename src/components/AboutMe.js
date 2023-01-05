01a7a561ff5b736397a7ffa69f9eb7ecb8dbee5d
Michael <m.paspaliaris@gmail.com>
2023-01-05 17:05:36 +1100

    changed content in AboutMe component

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -17,11 +17,11 @@ export default function AboutMe() {
             <div className="aboutme--container">
                 <div className="aboutme--content">
                     <h1>Over the past year<span>,</span></h1>
-                    <h3>After many years working in different industries, disciplines and countries, I finally found my calling and started my coding journey on April 1st, 2022. From my first lesson on April fools day, I found out that my love for coding was indeed not a joke.
+                    <h3>After many years working in different industries, disciplines and countries, in organsiations where I would often make presentations and reports where I needed extensive of use excel to help sort, filter & manage data I realised my passion and enthusiasm for all things technical. On April 1st, 2022, I decided to try my hand at coding, and from that day forward; even after many bugs, errors and issues, I found that my love for coding was indeed not a joke.
                         <br />
-                        From that day forward I have studied religiously never missing an opportunity to try and learn more in my newly adopted craft, from starting with html and css and then moving forward to deep dives into JavaScript, React and all the Libraries that come with it such as Redux. I have found a constant joy and passion with every new skill I've been able to learn and every app I've been able to build.
+                        Since that day forward I have studied religiously never missing an opportunity to try and learn more in my newly adopted craft, from starting with HTML and CSS and then moving forward to deep dives into JavaScript, React and all the Libraries that come with it such as Redux. I have found a constant joy and passion with every new skill I've been able to learn and every app I've been able to build.
                         <br />
-                        Every journey begins with a first step, and I am very fortunate to have taken that step as I wouldn't have been able to find a passion that allows me to be so creative whilst being able to also understand something so technical at the same time.
+                        Every journey begins with a first step, and I am very fortunate to have been able to find something that allows me to be so creative whilst being able to also understand and engineer something so technical at the same time.
                         <br />
                         Utilising learning platforms such as Codecademy and Scrimba I am entirely self-taught and self-motivated, nothing gives me more satisfaction than building a new app or idea, or finding the solution to that one bug in the code that has been paining me for hours.
                     </h3>
