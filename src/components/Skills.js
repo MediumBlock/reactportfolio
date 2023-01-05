1019d6a4b02149259e78eb51e045b6e5454159ae
Michael <m.paspaliaris@gmail.com>
2023-01-05 23:21:16 +1100

    made updates to responsive design to skills component

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -23,19 +23,19 @@ export default function Skills() {
                 <div className="skills--left">
                     {width > breakpoint && <img src={require("../resources/green-dots.png")}
                         className="green"
-                        alt="background image"
+                        alt=""
                     />}
                     <h1>Design</h1>
                     <h5>Experience in most aspects of visual design across multiple disciplines; such as colour theory, typography, visual hierachy, design philosophy and more.
                         <br /><br />Also experienced in achieving the best possible end user experience utilising UX topics such as Interaction / Inclusive Design, Design Psychology, as well as utilising tools and processes to help speed up and streamline the design process such as Wireframing and Prototyping
                         <br /><br />All the design principles listed above have not only been extensively studied but also put into practise across multiple projects showcased later on.</h5>
                     {width > breakpoint && <img src={require("../resources/skills-left.png")}
-                        alt="background image"
+                        alt=""
                     />}
                 </div>
                 <div className="skills--right">
                     {width > breakpoint && <img src={require("../resources/skills-right.png")}
-                        alt="background image"
+                        alt=""
                     />}
                     <h1>Engineering</h1>
                     <h5>What's good design without the means to implement? It has been crucial in my development to focus on sharpening my skills on all languages learnt throughout my studies with a particular focus on JavaScript, specifically, the React library.
@@ -45,7 +45,7 @@ export default function Skills() {
             <div>
                 <img src={require("../resources/line.png")}
                     className="squiggle--skills"
-                    alt="background image"
+                    alt=""
                 />
             </div>
         </motion.div>
