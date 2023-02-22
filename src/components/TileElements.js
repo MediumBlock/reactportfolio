d24185e5f877ba6bc4731329bb772d04fd24e4b0
Michael <m.paspaliaris@gmail.com>
2023-02-22 14:53:07 +1100

    minor update to responsive design in tileelements

--- src/components/TileElements.js
+++ src/components/TileElements.js
@@ -36,40 +36,42 @@ export default function TileElements() {
             className="elements--container"
         >
             {navBack}
-            <h2>{thisProject.name}</h2>
-            <div className="elements--content">
-                <div className="elements--text">
-                    <p>{thisProject.description.p1}</p>
-                    <br /><br />
-                    <p>{thisProject.description.p2}</p>
-                    <br /><br />
-                    <p>{thisProject.description.p3}</p>
-                    <br /><br />
-                    <p>{thisProject.description.p4}</p>
-                    {thisProject.description.p4 && <><br /><br /></>}
-                    <p>{thisProject.description.p5}</p>
-                    {thisProject.description.p5 && <><br /></>}
-                    <h4>Technologies and tools used:</h4>
-                    <ul>{thisProject.features.map(item => (<><li key={item}>{item}</li></>))}</ul>
-                    <h4>Approx time to complete:</h4>
-                    <p>{thisProject.time}</p>
-                    <h4>Link to project on GitHub <a href={thisProject.gitLink}
+            <div>
+                <h2>{thisProject.name}</h2>
+                <div className="elements--content">
+                    <div className="elements--text">
+                        <p>{thisProject.description.p1}</p>
+                        <br /><br />
+                        <p>{thisProject.description.p2}</p>
+                        <br /><br />
+                        <p>{thisProject.description.p3}</p>
+                        <br /><br />
+                        <p>{thisProject.description.p4}</p>
+                        {thisProject.description.p4 && <><br /><br /></>}
+                        <p>{thisProject.description.p5}</p>
+                        {thisProject.description.p5 && <><br /></>}
+                        <h4>Technologies and tools used:</h4>
+                        <ul>{thisProject.features.map(item => (<><li key={item}>{item}</li></>))}</ul>
+                        <h4>Approx time to complete:</h4>
+                        <p>{thisProject.time}</p>
+                        <h4>Link to project on GitHub <a href={thisProject.gitLink}
+                            target='_blank'
+                            rel="noopener"
+                            aria-label={thisProject.name}
+                        >
+                            here.
+                        </a></h4>
+
+                    </div>
+                    <a href={thisProject.link}
                         target='_blank'
                         rel="noopener"
-                        aria-label={thisProject.name}
-                    >
-                        here.
-                    </a></h4>
-
+                        aria-label={thisProject.name}>
+                        <img src={require(`../resources/${thisProject.thumbnail}`)}
+                            alt="project image"
+                        />
+                    </a>
                 </div>
-                <a href={thisProject.link}
-                    target='_blank'
-                    rel="noopener"
-                    aria-label={thisProject.name}>
-                    <img src={require(`../resources/${thisProject.thumbnail}`)}
-                        alt="project image"
-                    />
-                </a>
             </div>
 
         </motion.div>
